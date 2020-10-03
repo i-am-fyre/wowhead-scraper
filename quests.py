d683b1f8a7a863bda9df2b890690200e2d853c3b
Fyre-Homelab <58180427+Fyre-Homelab@users.noreply.github.com>
2020-10-02 22:54:06 -0500

    objectiveText1-4 implemented. details fixed.

--- quests.py
+++ quests.py
@@ -7,7 +7,7 @@ import csv
 category = ["quest"]
 language = ["en", "de","es","fr","it","pt","ru","ko","cn"]
 id_start = 1
-id_end = 100
+id_end = 1000
 
 for c in category:
     for i in range (id_start,id_end+1):
@@ -38,31 +38,43 @@ for c in category:
                         l = l[:-1]
 
                     # get id title
-                    title = soup.find("h1", {"class": "heading-size-1"}).text
+                    try:
+                        title = soup.find("h1", {"class": "heading-size-1"}).text
+                    except:
+                        title = ""
                     print("[" + l + "] title: " + title)
 
                     # get id details
-                    details = ""
-                    for d in soup.select('h2.heading-size-3')[0]:
-                        nextNode = d
-                        while True:
-                            nextNode = nextNode.next
-                            if nextNode is None:
-                                break
-                            if isinstance(nextNode, NavigableString):
-                                details += nextNode.strip()
-                            if isinstance(nextNode, Tag):
-                                if nextNode.name == "h2":
+                    try:
+                        details = ""
+                        for d in soup.select('h2.heading-size-3')[0]:
+                            nextNode = d
+                            while True:
+                                nextNode = nextNode.next
+                                if nextNode is None:
                                     break
-                                details += nextNode.get_text(strip=True).strip()
+                                if isinstance(nextNode, NavigableString):
+                                    details += nextNode.strip()
+                                if isinstance(nextNode, Tag):
+                                    if nextNode.name == "h2" or nextNode.name == "table":
+                                        break
+                                    details += nextNode.get_text(strip=True).strip()
+                    except:
+                        details = ""
                     print("[" + l + "] details: " + details)
 
                     # get id objectives
-                    objectives = soup.find("div", {"class": "block-block-bg is-btf"}).next_sibling.strip()
+                    try:
+                        objectives = soup.find("div", {"class": "block-block-bg is-btf"}).next_sibling.strip()
+                    except:
+                        objectives = ""
                     print("[" + l + "] objectives: " + objectives)
 
                     # get id offer reward text
-                    offerRewardText = soup.find("div", {"id": "lknlksndgg-completion"}).text.strip()
+                    try:
+                        offerRewardText = soup.find("div", {"id": "lknlksndgg-completion"}).text.strip()
+                    except:
+                        offerRewardText = ""
                     print("[" + l + "] offerRewardText: " + offerRewardText)
 
                     # get id requestItemsText
@@ -72,28 +84,44 @@ for c in category:
                         requestItemsText = ""
                     print("[" + l + "] requestItemsText: " + requestItemsText)
 
-                    # get id end text ---- NOT IMPLEMENTED YET
-                    endText = ""
+                    # get id end text  --   NOT IMPLEMENTED
+                    try:
+                        endText = "NOT IMPLEMENTED"
+                    except:
+                        endText = "NOT IMPLEMENTED"
                     print("[" + l + "] endText: " + endText)
 
-                    # get id objectiveText1 ---- NOT IMPLEMENTED YET
-                    objectiveText1 = ""
-                    print("[" + l + "] objectiveText1: " + objectiveText1)
+                    # get id objectiveText1
+                    try:
+                        objectiveText1 = soup.find("table", {"class": "iconlist"}).select("tr")[0].text
+                    except:
+                        objectiveText1 = ""
+                    print("[" + l + "] objectiveText1: " + str(objectiveText1))
 
-                    # get id objectiveText2 ---- NOT IMPLEMENTED YET
-                    objectiveText2 = ""
-                    print("[" + l + "] objectiveText2: " + objectiveText2)
+                    # get id objectiveText2
+                    try:
+                        objectiveText2 = soup.find("table", {"class": "iconlist"}).select("tr")[1].text
+                    except:
+                        objectiveText2 = ""
+                    print("[" + l + "] objectiveText2: " + str(objectiveText2))
 
-                    # get id objectiveText3 ---- NOT IMPLEMENTED YET
-                    objectiveText3 = ""
-                    print("[" + l + "] objectiveText3: " + objectiveText3)
+                    # get id objectiveText3
+                    try:
+                        objectiveText3 = soup.find("table", {"class": "iconlist"}).select("tr")[2].text
+                    except:
+                        objectiveText3 = ""
+                    print("[" + l + "] objectiveText3: " + str(objectiveText3))
 
-                    # get id objectiveText4 ---- NOT IMPLEMENTED YET
-                    objectiveText4 = ""
-                    print("[" + l + "] objectiveText4: " + objectiveText4)
+                    # get id objectiveText4
+                    try:
+                        objectiveText4 = soup.find("table", {"class": "iconlist"}).select("tr")[3].text
+                    except:
+                        objectiveText4 = ""
+                    print("[" + l + "] objectiveText4: " + str(objectiveText4))
+
 
                     #write to csv
                     csv_writer.writerow([i, title, details, objectives, offerRewardText, requestItemsText, endText, objectiveText1, objectiveText2, objectiveText3, objectiveText4])
             print("=========")
 
-print("DONE"),
+print("DONE")
